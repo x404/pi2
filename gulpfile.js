1cb941e4108d179930859fdf491ac182655b434d
Aleksey <kronos2003@gmail.com>
2019-03-24 13:18:00 +0200

    Добавлена страница "О нас"

--- gulpfile.js
+++ gulpfile.js
@@ -56,6 +56,7 @@ gulp.task('compress', function(){
 	pump([
 			gulp.src([  // Берем все необходимые библиотеки
 				config.templateDir + '/js/modernizr-custom-webp.js',
+				config.templateDir + '/js/modernizr.js',
 				config.libsDir + '/jquery/dist/jquery.js',
 				config.libsDir + '/jquery-validation/dist/jquery.validate.js',
 				config.templateDir + '/js/util.js',
