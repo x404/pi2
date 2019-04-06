eacd7ab3cf40b4d00fc8b44d4f259571a3cfd2c3
Aleksey <kronos2003@gmail.com>
2019-04-06 21:19:25 +0300

    IE11

--- app/template/js/engine.js
+++ app/template/js/engine.js
@@ -375,57 +375,7 @@ $(document).ready(function(){
 
 
 
-// popup on homepage
-class Popup{
-	constructor(name){
-		this.name = name;
-	}
-	open(){
-		// create overlay
-		const newOverlay = document.createElement('div');
-		newOverlay.className ='transparent-overlay';
-		document.querySelector('body').appendChild(newOverlay);
-		// show overlay
-		document.querySelector('.transparent-overlay').addEventListener('click', function(){
-			modal.close();
-		});
 
-		// GENERAL EVENT - ONKEYDOWN
-		document.onkeydown = function(evt) {
-		    evt = evt || window.event;
-		    let isEscape = false;
-		    if ("key" in evt) {
-		        isEscape = (evt.key == "Escape" || evt.key == "Esc");
-		    } else {
-		        isEscape = (evt.keyCode == 27);
-		    }
-		    if (isEscape) {
-		    	modal.close();
-		    }
-		};
-		// show popup
-		document.querySelector(this.name).classList.add('show');
-	}
-	close(){
-		document.querySelector('.transparent-overlay').remove();
-		// hide popup
-		document.querySelector(this.name).classList.remove('show');
-
-		// remove active class
-		document.querySelector('.cats .active').classList.remove('active')
-	}
-}
-
-
-[].forEach.call(document.querySelectorAll('[data-toggle="catmodal"]'),function(el,i){
-	el.addEventListener('click', function(e){
-		e.preventDefault();
-		let target = el.dataset.target;
-			modal = new Popup(target);
-			modal.open();
-			this.classList.add('active')
-		})
-})
 
 
 
